9a2ad7c4955f0caed693bf763ea5b41faff612e8
Chet Kener <Cl3Kener@gmail.com>
2014-09-12 13:54:05 -0400

    Add Themes to Vendor
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -54,6 +54,9 @@ PRODUCT_COPY_FILES += \
     vendor/purity/prebuilt/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
     vendor/purity/prebuilt/apk/Superuser.apk:system/app/Superuser.apk
 
+# T-Mobile theme engine
+include vendor/purity/config/themes_common.mk
+
 # Enable SIP+VoIP
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
